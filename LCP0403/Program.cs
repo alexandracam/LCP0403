fdacc5a34be562f2ee24f8a6fe0011f0b9a64215
Alexandra Camarena <alexandracamarena@ou.edu>
2024-04-01 20:08:19 -0500

    Add project files.

---
+++ LCP0403/Program.cs
@@ -0,0 +1,32 @@
+﻿// MIS 3013 001
+// April 3, 2024
+// Alexandra Camarena
+// 113568154
+
+// 
+Console.WriteLine("class");
+// cog: 80/each
+// gear: 200/each
+Order or1;
+or1 = new Order();
+or1.nCogs = 1;
+or1.nGears = 2;
+
+or1.subtotal = or1.CalSubtotal();
+
+Console.WriteLine($"The subtotal is {or1.subtotal:C2}");
+
+// end of your code
+class Order
+{ 
+    public int nCogs;// by defualt, it is private
+    public int nGears;
+    public double subtotal;
+
+    public double CalSubtotal() 
+    {
+        double r;
+        r = 80 * nCogs + 200 * nGears;
+        return r;
+    }
+}
